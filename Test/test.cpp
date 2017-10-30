35241fb6543849007ede37e5d076c565b13dc0cc
littesss <liushaohua_2017@163.com>
2017-10-30 08:15:11 +0800

    test to update the repositories

---
+++ Test/test.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Created Time: Mon 30 Oct 2017 08:11:55 AM CST
+ ************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello wrold" << endl;
+    return 0;
+}
