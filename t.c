e850ba2aac43386fd9360a3082d0dcbd1eb06120
littesss <liushaohua_2017@163.com>
2018-01-26 00:59:34 +0800

    modify the struct file ,and goodnightgit status!

--- t.c
+++ t.c
@@ -15,15 +15,16 @@ int add(int a, int b)
     return a + b;
 }
 
-int add1(num *n)
+int add1(num *n)            //传址才能改变全局变量
 {
-    (*n).a = 100;
-    (*n).b = 200;
+    n->a = 100;             //指针所指的值
+    (*n).b = 200;           //如上
     return (*n).a+(*n).b;
 }
 /* 全局变量，结构体num */
     num n = {10, 20};
 
+/* 全局变量不能如下初始化, 如果不初始化，系统就会初始化为0，要么一次性初始化 */
    // n.a = 10;
    // n.b = 20;
 /* main函数 */    
@@ -35,7 +36,6 @@ int main()
     sum = add(n.a, n.b);
     int sum1 = add1(&n);
     printf("sum = %d\n", sum);
-    
     
     printf("n.a = %d, n.b = %d\n", n.a, n.b);
     printf("sum1 = %d\n", sum1);
